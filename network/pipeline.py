91c6cba2f46b57092791ec9441ee3d96b52926eb
yamaru12345 <65715903+yamaru12345@users.noreply.github.com>
2021-07-09 10:04:39 +0900

    Update pipeline.py

--- network/pipeline.py
+++ network/pipeline.py
@@ -24,7 +24,6 @@ class PipelineFlownet:
 		self.network = build_network(getattr(config.network, 'class').get('MaskFlownet'))(config=config)
 		self.network.hybridize()
 		self.network.collect_params().initialize(init=mx.initializer.MSRAPrelu(slope=0.1), ctx=self.ctx)
-		print(self.network.collect_params())
 		self.network.collect_params('(maskflownet0_maskflownet_s0).*').setattr('grad_req', 'null')
 		self.network.collect_params('(maskflownet0_hybridsequential).*').setattr('grad_req', 'null')
 		self.trainer = gluon.Trainer(self.network.collect_params(), 'adam', {'learning_rate': 1e-5})
